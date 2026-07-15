d86da378e5d7680f12df5fca017a7625f9dcb190
SrOH-cpu <dswehrli@gmx.ch>
2026-07-15 15:50:40 +0200

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('health/', views.health_check, name='health_check'),
+    # ... your other routes
+]
